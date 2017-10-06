d16be8fa90286a7fddc13c755d685f244c13d6c6
hujian06 <hujian06@meituan.com>
2017-10-06 13:24:06 +0800

    from hujian #2017-10-06 13:23# remove some files.

--- src/test/java/io/hujian/rpc/test/client/HelloService.java
+++ src/test/java/io/hujian/rpc/test/client/HelloService.java
@@ -1,5 +1,5 @@
 package io.hujian.rpc.test.client;
 
 public interface HelloService {
-    String hello(String name);
+    String hello(String msg);
 }
--- src/test/java/io/hujian/rpc/test/server/HelloServiceImpl.java
+++ src/test/java/io/hujian/rpc/test/server/HelloServiceImpl.java
@@ -7,8 +7,8 @@ import io.hujian.rpc.test.client.HelloService;
 @RpcService(HelloService.class)
 public class HelloServiceImpl implements HelloService {
     @Override
-    public String hello(String name) {
-        return "Hello! " + name;
+    public String hello(String msg) {
+        return "say hello o:" + msg;
     }
 
 }
